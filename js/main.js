e203d75a6a50cd927fc78dae999d8649fee5ffce
sulthanbasha554@gmail.com <sulthanbasha554@gmail.com>
2019-09-09 19:49:32 +0530

    FIX UI Issues

--- js/main.js
+++ js/main.js
@@ -51,39 +51,7 @@
       $('#header').addClass('header-scrolled');
     }
 
-    // Smooth scroll for the menu and links with .scrollto classes
-    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        if (target.length) {
-          var top_space = 0;
 
-          if ($('#header').length) {
-            top_space = $('#header').outerHeight();
-
-            if (! $('#header').hasClass('header-scrolled')) {
-              top_space = top_space - 20;
-            }
-          }
-
-          $('html, body').animate({
-            scrollTop: target.offset().top - top_space
-          }, 1500, 'easeInOutExpo');
-
-          if ($(this).parents('.nav-menu').length) {
-            $('.nav-menu .menu-active').removeClass('menu-active');
-            $(this).closest('li').addClass('menu-active');
-          }
-
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-            $('#mobile-body-overly').fadeOut();
-          }
-          return false;
-        }
-      }
-    });
 
     // Navigation active state on scroll
     var nav_sections = $('section');
